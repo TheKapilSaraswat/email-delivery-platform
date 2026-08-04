73792f72d1669a3b04b4309560fdcc21f926daa0
Kapil Saraswat <kapil.saraswat981@gmail.com>
2026-08-05 01:36:26 +0530

    fix: /swagger returns 302 redirect (use @Controller not @RestController for redirect view)

--- backend/src/main/java/com/emailplatform/config/OpenApiConfig.java
+++ backend/src/main/java/com/emailplatform/config/OpenApiConfig.java
@@ -7,8 +7,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 public class OpenApiConfig {
@@ -29,7 +29,7 @@ public class OpenApiConfig {
                                 .bearerFormat("JWT")));
     }
 
-    @RestController
+    @Controller
     static class SwaggerRedirectController {
 
         @GetMapping({"/swagger", "/swagger/"})
